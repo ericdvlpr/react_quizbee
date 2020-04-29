dfc62217c763347f6b04c497049730e9026ba127
Eric Jaucian <gitemail@shoperones.com>
2020-04-29 16:24:19 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import "./assets/style.css";
+import ReactDOM from "react-dom";
+import quizService from "./quizService";
+import QuestionBox from "./components/QuestionBox";
+import Result from "./components/Result";
+
+class QuizBee extends Component {
+  state={
+    questionBank:[],
+    score: 0,
+    responses:0
+  };
+  getQUestions = () =>{
+    quizService().then(question=>{
+      this.setState({
+        questionBank:question
+      });
+    });
+  };
+  computerAnswer = (answer, correctAnswer) =>{
+    if(answer === correctAnswer){
+      this.setState({
+        score: this.state.score + 1
+      });
+    }
+    this.setState({
+      responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+    })
+  }
+  playAgain = () =>{
+    this.getQUestions();
+    this.setState({
+      score: 0,
+      responses:0
+    })
+  }
+
+  componentDidMount(){
+    this.getQUestions();
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="title">QuizBee</div>
+    {this.state.questionBank.length > 0 && 
+    this.state.responses < 5 &&
+    this.state.questionBank.map(({question,answers,correct,questionId})=>(
+      <QuestionBox question={question} options={answers} key={questionId} selected = {answer => this.computerAnswer(answer,correct)} />
+    )
+    )}
+
+        {this.state.responses === 5 ? (<Result score={this.state.score} playAgain={this.playAgain} />): null}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<QuizBee />, document.getElementById("root"));
+
